55c69d7799b5c8594240d45f57afda0231817111
farukayaz <frkyz07.13@gmail.com>
2022-08-03 18:52:29 +0300

    - project structure finished

--- src/test/java/devices/DeviceFarm.java
+++ src/test/java/devices/DeviceFarm.java
@@ -1,4 +1,11 @@
 package devices;
 
-public class DeviceFarm {
+public enum DeviceFarm {
+    ANDROID_OREO("src/test/resources/capabilities/android-oreo.json"),
+    ANDROID_KITKAT("src/test/resources/capabilities/android-kitkat.json");
+
+    public  String path;
+    DeviceFarm(String path) {
+        this.path = path;
+    }
 }
--- src/test/java/testng/ContactManagerTestNG.java
+++ src/test/java/testng/ContactManagerTestNG.java
@@ -1,4 +1,61 @@
 package testng;
 
+import devices.DeviceFarm;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.AddContactPage;
+import pages.HomePage;
+import utility.DeviceFarmUtility;
+
+import javax.swing.text.Utilities;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 public class ContactManagerTestNG {
+
+    public static AppiumDriver<?> Driver;
+    HomePage homePage;
+    AddContactPage addContactPage;
+    String oreo, kitkat;
+    DesiredCapabilities capabilities;
+
+    public ContactManagerTestNG() {
+        oreo = DeviceFarm.ANDROID_OREO.path;
+        kitkat = DeviceFarm.ANDROID_KITKAT.path;
+
+    }
+    @BeforeClass
+    public void setup() throws MalformedURLException {
+
+        capabilities = new DesiredCapabilities();
+        capabilities = DeviceFarmUtility.pathToDesiredCapabilitites(this.oreo);
+        capabilities.setCapability("app", new File("src/test/resources/apps/ContactManager.apk").getAbsolutePath());
+        Driver = new AndroidDriver(new URL("http://127.0.0.1:8080/wd/hub"), capabilities);
+        homePage = new HomePage();
+        addContactPage = new AddContactPage();
+    }
+    @Test(priority = 0)
+    public void openAddContactOnOreo() throws NullPointerException, InterruptedException {
+
+        homePage.getAddContactBtn().click();
+        Thread.sleep(4000);
+    }
+    @Test(priority = 1)
+    public void checkAddContactTitle(){
+        Assert.assertEquals(addContactPage.getTitle().getText(),"Add Contact");
+    }
+    @AfterClass
+    public void waiter() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
 }
